5b1192cc692215258bf93cfead59d6ddc598b33e
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-08 22:05:29 -0600

    Literales de cadenas.

---
+++ cadenas.py
@@ -0,0 +1,11 @@
+print("Literales de cadenas:")
+
+print("Me gusta Monty Python")
+print("Me gusta \"Monty Python\"")
+print('Me gusta \"Monty Python\"')
+print('Me gusta "Monty Python"')
+print()
+
+print('I\'m Monty Python')
+print("I\'m Monty Python")
+print("I'm Monty Python")
