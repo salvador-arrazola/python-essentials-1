fa13379ed6bfff847ffb5ba35f1cd8c17f61fce7
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-08 22:10:47 -0600

    Literales de booleanos.

---
+++ booleanos.py
@@ -0,0 +1,16 @@
+print("Literales de booleanos:")
+
+print(True)
+print(False)
+print()
+
+print(True, type(True))
+print(False, type(False))
+print()
+
+print(True > False)
+print(True < False)
+print(True, int(True))
+print(False, int(False))
+print()
+
