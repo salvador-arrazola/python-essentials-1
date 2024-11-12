15586eebb3ed104e4a1c3a942fa7459f044a1e8e
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-12 15:56:09 -0600

    Revert "Agrega ejemplos de la funcion input."
    This reverts commit 52c499afab27784a38c30f3397a7534d6577ee46.

--- input.py
+++
@@ -1,7 +0,0 @@
-print("Dime lo que sea:")
-anything = input()
-print("Hmm...", anything, "... ¿en serio?")
-print()
-
-anything = input("Dime lo que sea: ")
-print("Hmm...", anything, "...¿en serio?")
--- input2.py
+++
@@ -1,5 +0,0 @@
-# input siempre lee la entrada del usuario como cadena.
-# El siguiente codigo generar un error TypeError.
-anything = input("Ingresa un número: ")
-something = anything ** 2.0
-print(anything, "al cuadrado es", something)
