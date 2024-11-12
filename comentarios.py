3f335d0feb9c29a753a174d0524b64728650bf78
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-12 15:28:35 -0600

    Agrega ejemplo del uso de comentarios.

---
+++ comentarios.py
@@ -0,0 +1,10 @@
+# Calcula los segundos en n horas.
+
+horas = 2
+segundos_por_hora = 3600
+
+print("Horas: ", horas)
+print("Segundos en", horas, "horas:", horas * segundos_por_hora)
+print()
+
+print("Hasta la vista, Baby !!!")
