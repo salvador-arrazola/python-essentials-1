52c499afab27784a38c30f3397a7534d6577ee46
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-12 15:44:39 -0600

    Agrega ejemplos de la funcion input.

---
+++ input.py
@@ -0,0 +1,7 @@
+print("Dime lo que sea:")
+anything = input()
+print("Hmm...", anything, "... ¿en serio?")
+print()
+
+anything = input("Dime lo que sea: ")
+print("Hmm...", anything, "...¿en serio?")
---
+++ input2.py
@@ -0,0 +1,5 @@
+# input siempre lee la entrada del usuario como cadena.
+# El siguiente codigo generar un error TypeError.
+anything = input("Ingresa un número: ")
+something = anything ** 2.0
+print(anything, "al cuadrado es", something)
