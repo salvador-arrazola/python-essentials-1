418ac2d230df6cf4008c0d948c3f14ff9a862dc0
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-11-12 17:01:16 -0600

    Agrega ejemplos del uso de la concatenacion y la replicacion de cadenas.

---
+++ concatenacion.py
@@ -0,0 +1,4 @@
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Ahora, ingresa tu apellido: ")
+print("Gracias. ")
+print("Tu nombre completo es: " + nombre +" "+ apellido +".")
---
+++ replicacion.py
@@ -0,0 +1,4 @@
+# Imprime un cuadrado haciendo uso de la replicacion de cadenas.
+print("+" + 10 * "-" + "+")
+print(("|" + " " * 10 + "|\n") * 5, end="")
+print("+" + 10 * "-" + "+")
